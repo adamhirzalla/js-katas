ded90675311b47aa50b2de2a09c7c43e9e597a63
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-14 14:38:04 -0400

    Solving Kata 8: Repeating Numbers

---
+++ kata8.js
@@ -0,0 +1,20 @@
+const repeatNumbers = function (data) {
+  let sol = ""; // string containing all repeated strings of arrays
+  let length = 0; // # of array of numbers done
+  for (const array of data) {
+    for (let i = 0; i < array[1]; i++) {
+      sol += array[0];
+    }
+    length++; // finished repeating for +1 array
+    if (length === data.length) // last array was just repeated -> leave
+      return sol;
+    sol += ', '; // prepare for next array
+  }
+};
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+// 1111111111
+// 11, 222
+// 10101010, 343434343434, 9292
