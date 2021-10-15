c72d07dd05deb2028c9b5ade24e7714ca9addc5b
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-10-15 18:41:32 -0400

    Solving Kata 15 - Organizing Instructors

---
+++ kata15.js
@@ -0,0 +1,32 @@
+const organizeInstructors = function (instructors) {
+  const classes = {};
+  for (const instructor of instructors) {
+    if (classes[instructor.course] === undefined)
+      classes[instructor.course] = new Array;
+    classes[instructor.course].push(instructor.name);
+  }
+  return classes;
+};
+
+console.log(organizeInstructors([
+  { name: "Samuel", course: "iOS" },
+  { name: "Victoria", course: "Web" },
+  { name: "Karim", course: "Web" },
+  { name: "Donald", course: "Web" }
+]));
+console.log(organizeInstructors([
+  { name: "Brendan", course: "Blockchain" },
+  { name: "David", course: "Web" },
+  { name: "Martha", course: "iOS" },
+  { name: "Carlos", course: "Web" }
+]));
+
+// {
+//   iOS: ["Samuel"],
+//   Web: ["Victoria", "Karim", "Donald"]
+// }
+// {
+//   Blockchain: ["Brendan"],
+//   Web: ["David", "Carlos"],
+//   iOS: ["Martha"]
+// }
